fc6e4ea44058436a85495a4d16a6654af3ff2785
Lenovo <13551439192@163.com>
2025-05-08 12:47:08 +0800

    学习了Button的建立

--- app/src/main/java/com/example/compose_study/MainActivity.kt
+++ app/src/main/java/com/example/compose_study/MainActivity.kt
@@ -1,28 +1,43 @@
                                                                                                                      package com.example.compose_study
 
+import android.R.attr.background
+import android.R.attr.text
+import android.R.attr.textColor
 import android.content.res.Configuration
-import android.graphics.Color
+
 import android.os.Bundle
 import android.provider.Telephony
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.Button
 import androidx.compose.animation.animateContentSize
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -42,16 +57,19 @@ import com.example.compose_study.ui.theme.Compose_studyTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
       //  enableEdgeToEdge()
         setContent {
             Compose_studyTheme {
                // MessageCard(Message("Jetpack Compose","study"))
                 Conversation(messages = MsgData.messages)
+                ButtonDemo()
             }
         }
     }
     }
        data class Message(val author: String,val body: String)
+       data class ButtonState(var text: String,var textColor: Color,var buttonColor: Color)
      @Composable
      fun MessageCard(msg: Message){
          var isExpanded by remember { mutableStateOf(false) }
@@ -97,9 +115,33 @@ class MainActivity : ComponentActivity() {
                     MessageCard(msg = messages)
                 }
             }}
+           @Composable
 
-       @Preview(name = "Light Mode")
-       @Preview(
+           fun ButtonDemo(){
+
+             val interactionState =remember { MutableInteractionSource() }
+               val(text,textColor,buttonColor)=when{
+                   interactionState.collectIsPressedAsState().value->ButtonState("Just Pressed",Color.Red , Color.Black)
+                   else->ButtonState("Just Button", Color.White, Color.Red)
+               }
+               Button(onClick = {/*TODO*/},
+                   interactionSource = interactionState,
+                   elevation = null,
+                   shape = RoundedCornerShape(50),
+                   colors = ButtonDefaults.buttonColors(
+                       containerColor = buttonColor
+                   ),
+                   modifier = Modifier.width(IntrinsicSize.Min).height(IntrinsicSize.Min)
+                   ){Text(
+                       text=text, color = textColor
+                   )
+
+               }
+           }
+
+
+
+           @Preview(
            uiMode = Configuration.UI_MODE_NIGHT_YES,
            showBackground = true,
            name="Dark Mode"
